3451cc7a28d26bd5a25c54b79b02c6ae899f65b8
John Murray <johnmurray@fastmail.com>
2022-11-29 08:29:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Using @container
+
+[A Jack Herrington tutorial](https://www.youtube.com/watch?v=ypN-Uwshc5M)
+
+## Getting started
+
+1. `yarn create vite css-container --template react`
+2. `cd css-container`
+3. `yarn`
+4. `yarn dev`
---
+++ css-container/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="layout">
+        <main>
+          <h1>Main</h1>
+        </main>
+        <article>
+          <h1>Article</h1>
+        </article>
+      </div>
+    </div>
+  );
+}
+
+export default App;
